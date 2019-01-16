06d0a9f94ea9671373bcb5ec2aec83591c4b2031
MortarTimmyMonkey <46425741+MortarTimmyMonkey@users.noreply.github.com>
2019-01-16 14:25:13 +0800

    Update tooshie.py

--- tooshie.py
+++ tooshie.py
@@ -14,5 +14,20 @@ async def ping(ctx):
     ping_ = bot.latency
     ping = round(ping_ * 1000)
     await ctx.channel.send(f"Pong! {ping}ms")
+
+@bot.command()
+async def ban(ctx, member:discord.User = None, reason = None):
+    if member == ctx.message.author:
+        await ctx.channel.send("You cannot ban yourself!")
+        return
+    if member == None:
+        await ctx.channel.send("No member at all!")
+        return
+    if reason == None:
+        reason = "No reason at all!"
+    message = f"You have been banned from {ctx.guild.name} for {reason}!"
+    await member.send(message)
+    await ctx.guild.ban(member)
+    await ctx.channel.send(f"{member} is banned!")
     
 bot.run(os.getenv('TOKEN'))
